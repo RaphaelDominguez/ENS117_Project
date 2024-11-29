0ca2bfb8877d4c7171bd60ed6c79c5440d56f819
Raphael Dominguez <54604698+RaphaelDominguez@users.noreply.github.com>
2024-11-29 18:51:40 -0500

    Add files via upload

---
+++ ENS117_Project.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+
+
+df = pd.read_csv()
